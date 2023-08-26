2a6c31eda8722d87f39440759cf6f2f6361dda9a
YevhenP <panchoshnuyevgeniy@outlook.com>
2023-08-26 12:13:58 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,8 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-
-import { HomeComponent } from "./home/home.component";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [CommonModule, RouterModule, HomeComponent],
-  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img
-            class="brand-logo"
-            src="assets/logo.svg"
-            alt="logo"
-            aria-hidden="true"
-          />
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
-  styles: [
-    `
-      :host {
-        --content-padding: 10px;
-      }
-      header {
-        display: block;
-        height: 60px;
-        padding: var(--content-padding);
-        box-shadow: 0px 5px 25px var(--shadow-color);
-      }
-      .content {
-        padding: var(--content-padding);
-      }
-    `,
-  ],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = "homes";
-}
+export class AppComponent {}
